1691d8c060129066500f788ab87703198ecd4547
Tublanx <rlgus03453@naver.com>
2025-06-20 00:28:20 +0900

    ip주소에 http 추가

--- app/src/main/java/com/lgh/wordskill2026ex/config/RetrofitClientConfig.java
+++ app/src/main/java/com/lgh/wordskill2026ex/config/RetrofitClientConfig.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientConfig {
 
-    private static final String BASE_URL = "10.0.2.2:8080";
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
 
     public static ApiService create() {
         // Retrofit 객체 생성
