6d90903f8a22d280d3afe532432c9e2bf2ce43dd
Tublanx <rlgus03453@naver.com>
2025-06-19 19:16:02 +0900

    로그인 요청/응답 메서드 추가

--- app/src/main/java/com/lgh/wordskill2026ex/api/ApiService.java
+++ app/src/main/java/com/lgh/wordskill2026ex/api/ApiService.java
@@ -1,4 +1,14 @@
 package com.lgh.wordskill2026ex.api;
 
+import com.lgh.wordskill2026ex.response.LoginResponse;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface ApiService {
+
+    @POST("/api/login")
+    Call<LoginResponse> login(@Query("email") String email, @Query("password") String password);
+
 }
