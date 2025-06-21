7a7a0f7db1374bec251214c705ac18138c6bf59c
Tublanx <rlgus03453@naver.com>
2025-06-21 22:15:45 +0900

    이메일 중복 여부 확인하는 로직 축라

--- app/src/main/java/com/lgh/wordskill2026ex/activity/RegisterActivity.java
+++ app/src/main/java/com/lgh/wordskill2026ex/activity/RegisterActivity.java
@@ -2,59 +2,89 @@ package com.lgh.wordskill2026ex.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.lgh.wordskill2026ex.R;
 import com.lgh.wordskill2026ex.api.ApiService;
 import com.lgh.wordskill2026ex.config.RetrofitClientConfig;
+import com.lgh.wordskill2026ex.response.ValidateEmailResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText nameEditText;
+    EditText emailEditText;
+    EditText passwordEditText;
+    Button signUpBtn;
+    Button googleLoginBtn;
+    TextView gotoLoginText;
+    TextView gotoMainText;
+
     private final ApiService apiService = RetrofitClientConfig.create();
+    private final String regEmail = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private final String regPwd = "^(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\"'<>,.?/]).{8,}$";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        EditText nameEditText = findViewById(R.id.nameEditText);
-        EditText emailEditText = findViewById(R.id.signUp_emailEditText);
-        EditText passwordEditText = findViewById(R.id.signUp_passwordEditText);
-        Button signUpBtn = findViewById(R.id.signUpButton);
-        Button googleLoginBtn = findViewById(R.id.signUp_googleSignUpButton);
+        nameEditText = findViewById(R.id.nameEditText);
+        emailEditText = findViewById(R.id.signUp_emailEditText);
+        passwordEditText = findViewById(R.id.signUp_passwordEditText);
+        signUpBtn = findViewById(R.id.signUpButton);
+        googleLoginBtn = findViewById(R.id.signUp_googleSignUpButton);
         TextView gotoLoginText = findViewById(R.id.signUp_gotoLoginText);
         TextView gotoMainText = findViewById(R.id.signUp_gotoMainText);
-
-        String regEmail = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-        String regPwd = "^(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\"'<>,.?/]).{8,}$";
 
         // 계정 생성 버튼 클릭 시
         signUpBtn.setOnClickListener(e -> {
             if (nameEditText.getText().toString().trim().isEmpty()) {
-                Toast.makeText(getApplicationContext(), "이름을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                Snackbar.make(e, "이름을 입력해주세요", Snackbar.LENGTH_SHORT).show();
                 return;
             }
 
             if (emailEditText.getText().toString().isEmpty() || !emailEditText.getText().toString().trim().matches(regEmail)) {
-                Toast.makeText(getApplicationContext(), "이메일을 형식에 맞게 입력해주세요.", Toast.LENGTH_SHORT).show();
+                Snackbar.make(e, "이메일을 형식에 맞게 입력해주세요.", Snackbar.LENGTH_SHORT).show();
                 return;
             }
 
-            if (passwordEditText.getText().toString().isEmpty()) {
-                Toast.makeText(getApplicationContext(), "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
-                return;
-            }
+            apiService.validateEmail(emailEditText.getText().toString().trim()).enqueue(new Callback<ValidateEmailResponse>() {
+                @Override
+                public void onResponse(@NonNull Call<ValidateEmailResponse> call, @NonNull Response<ValidateEmailResponse> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        boolean success = response.body().isSuccess();
 
-            if (!passwordEditText.getText().toString().trim().matches(regPwd)) {
-                Toast.makeText(getApplicationContext(), "비밀번호는 8자 이상, 특수문자 1개 이상 포함되어야합니다.", Toast.LENGTH_SHORT).show();
-                return;
-            }
+                        if (!success) {
+                            Snackbar.make(e, "이미 사용 중인 이메일입니다.", Snackbar.LENGTH_SHORT).show();
+                            return;
+                        } else {
+                            signUp(e);
+                        }
+                    } else {
+                        Snackbar.make(e, "이미 사용 중인 이메일입니다.", Snackbar.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+
+                @Override
+                public void onFailure(@NonNull Call<ValidateEmailResponse> call, @NonNull Throwable t) {
+                    Snackbar.make(e, "통신에 실패했습니다. 다시 시도해주세요.", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+            });
         });
 
         // 구글 회원가입 버튼 클릭 시
@@ -73,5 +103,17 @@ public class RegisterActivity extends AppCompatActivity {
         gotoMainText.setOnClickListener(e -> {
 
         });
+    }
+
+    private void signUp(View e) {
+        if (passwordEditText.getText().toString().isEmpty()) {
+            Snackbar.make(e, "비밀번호를 입력해주세요.", Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!passwordEditText.getText().toString().trim().matches(regPwd)) {
+            Toast.makeText(getApplicationContext(), "비밀번호는 8자 이상, 특수문자 1개 이상 포함되어야합니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
--- app/src/main/java/com/lgh/wordskill2026ex/api/ApiService.java
+++ app/src/main/java/com/lgh/wordskill2026ex/api/ApiService.java
@@ -1,6 +1,7 @@
 package com.lgh.wordskill2026ex.api;
 
 import com.lgh.wordskill2026ex.response.LoginResponse;
+import com.lgh.wordskill2026ex.response.ValidateEmailResponse;
 
 import retrofit2.Call;
 import retrofit2.http.POST;
@@ -10,5 +11,8 @@ public interface ApiService {
 
     @POST("/api/login")
     Call<LoginResponse> login(@Query("email") String email, @Query("password") String password);
+
+    @POST("/api/validateEmail")
+    Call<ValidateEmailResponse> validateEmail(@Query("email") String email);
 
 }
