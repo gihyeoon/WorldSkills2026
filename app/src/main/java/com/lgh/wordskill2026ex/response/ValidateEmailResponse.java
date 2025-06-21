b3d8016269d42dab56aad980b67d30c4f345009c
Tublanx <rlgus03453@naver.com>
2025-06-21 22:15:36 +0900

    이메일 중복 확인 응답 DTO 파일 추가

---
+++ app/src/main/java/com/lgh/wordskill2026ex/response/ValidateEmailResponse.java
@@ -0,0 +1,22 @@
+package com.lgh.wordskill2026ex.response;
+
+public class ValidateEmailResponse {
+
+    private boolean success;
+
+    private String message;
+
+    private int code;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
