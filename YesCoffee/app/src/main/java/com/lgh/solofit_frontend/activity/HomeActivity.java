1df87e9e83c438bb73174a36e21c0a1bd406bf51
Tublanx <rlgus03453@naver.com>
2025-08-11 21:32:33 +0900

    나와 가까운 매장 리스트 보여주는 것까지 완성

--- YesCoffee/app/src/main/java/com/lgh/solofit_frontend/activity/HomeActivity.java
+++ YesCoffee/app/src/main/java/com/lgh/solofit_frontend/activity/HomeActivity.java
@@ -1,11 +1,20 @@
 package com.lgh.solofit_frontend.activity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.lgh.solofit_frontend.R;
+import com.lgh.solofit_frontend.dto.Market;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,5 +22,29 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        LinearLayout marketListLayout = findViewById(R.id.llMarketList);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        List<Market> marketList = new ArrayList<>();
+
+        marketList.add(new Market("댄싱컵 수영현대점", 5, R.drawable.coffee_icon));
+        marketList.add(new Market("컴포즈커피 센텀비스타점", 0, R.drawable.coffee_icon));
+        marketList.add(new Market("텐퍼센트커피 수영현대점", 5, R.drawable.coffee_icon));
+        marketList.add(new Market("카페051 민락현대점", 11, R.drawable.coffee_icon));
+
+        for (Market m : marketList) {
+            View view = inflater.inflate(R.layout.item_main_market_list, marketListLayout, false);
+
+            ImageView marketImg = view.findViewById(R.id.ivMarketImg);
+            TextView marketName = view.findViewById(R.id.tvMarketName);
+            TextView marketTime = view.findViewById(R.id.tvGetTime);
+
+            marketImg.setImageResource(m.imageResId);
+            marketName.setText(m.marketName);
+            marketTime.setText(m.time == 0 ? "바로 수령 가능" : m.time + "분 후 수령");
+
+            marketListLayout.addView(view);
+        }
     }
 }
